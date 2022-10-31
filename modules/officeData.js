2a46d758b7489fcfb9afc44b4d4a55780049fc0a
Reza Karzari <contact@rezakarzari.com>
2022-10-31 15:22:48 -0400

    first commit

---
+++ modules/officeData.js
@@ -0,0 +1,82 @@
+class Data {
+  employees;
+  classes;
+
+  constructor(employees, classes) {
+    this.employees = employees;
+    this.classes = classes;
+  }
+}
+
+let dataCollection = null;
+
+module.exports.initialize = () => {
+  const fs = require("fs");
+  return new Promise((resolve, reject) => {
+    fs.readFile("./data/employees.json", "utf8", (err, dataEmployees) => {
+      if (err) {
+        reject("unable to read employees.json");
+      } else {
+        fs.readFile("./data/classes.json", "utf8", (err, dataClasses) => {
+          if (err) {
+            reject("unable to read classes.json");
+            return;
+          } else {
+            dataCollection = new Data(
+              JSON.parse(dataEmployees),
+              JSON.parse(dataClasses)
+            );
+            resolve();
+          }
+        });
+      }
+    });
+  });
+};
+
+module.exports.getAllEmployees = () =>
+  new Promise((resolve, reject) => {
+    dataCollection.employees.length === 0
+      ? reject("no results returned")
+      : resolve(dataCollection.employees);
+  });
+
+module.exports.getEAs = () =>
+  new Promise((resolve, reject) =>
+    dataCollection.employees.filter((employee) => employee.EA).length === 0
+      ? reject("no results returned")
+      : resolve(dataCollection.employees.filter((employee) => employee.EA))
+  );
+
+module.exports.getClasses = () =>
+  new Promise((resolve, reject) =>
+    dataCollection.classes.length === 0
+      ? reject("no results returned")
+      : resolve(dataCollection.classes)
+  );
+
+module.exports.getPartTimers = () =>
+  new Promise((resolve, reject) =>
+    dataCollection.employees.filter(
+      (employee) => employee.status === "Part Time"
+    ).length === 0
+      ? reject("no results returned")
+      : resolve(
+          dataCollection.employees.filter(
+            (employee) => employee.status === "Part Time"
+          )
+        )
+  );
+
+module.exports.getEmployeeByNum = (num) =>
+  new Promise((resolve, reject) =>
+    dataCollection.employees.find(
+      (employee) => employee.employeeNum === num
+    ) === undefined
+      ? reject("no results returned")
+      : resolve(
+          dataCollection.employees.find(
+            (employee) => employee.employeeNum === num
+          )
+        )
+  );
---
+++ server.js
@@ -0,0 +1,79 @@
+/*********************************************************************************
+ * WEB322 – Assignment 3
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Reza Karzari Student ID: 153845201  Date: 10/12/2022
+ *
+ *
+ ********************************************************************************/
+
+var express = require("express");
+var app = express();
+var HTTP_PORT = process.env.PORT || 8080;
+var path = require("path");
+
+const {
+  initialize,
+  getPartTimers,
+  getEmployeeByNum,
+} = require("./modules/officeData");
+
+// setup a 'route' to listen on the default url path
+
+app.use(express.static("public"));
+
+app.get("/PartTimer", (req, res) => {
+  getPartTimers()
+    .then((value) => res.send(JSON.stringify(value)))
+    .catch((error) => res.send(JSON.stringify({ message: error })));
+});
+app.get("/employee/:num", (req, res) => {
+  getEmployeeByNum(Number(req.params.num))
+    .then((value) => res.send(JSON.stringify(value)))
+    .catch((error) => res.send(JSON.stringify({ message: error })));
+});
+app.get("/PartTimer", (req, res) => {
+  getPartTimers()
+    .then((value) => res.send(JSON.stringify(value)))
+    .catch((error) => res.send(JSON.stringify({ message: error })));
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+app.get("/audio", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/audio.html"));
+});
+app.get("/video", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/video.html"));
+});
+app.get("/table", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/table.html"));
+});
+app.get("/list", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/list.html"));
+});
+app.get("/storefront", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/storefront.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+
+// setup http server to listen on HTTP_PORT
+
+
+initialize()
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log("server listening on port: " + HTTP_PORT);
+    });
+  })
+  .catch((error) => console.log(error));
+
